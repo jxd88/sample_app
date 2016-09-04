aa5c141489f7f00c6064dd60fc4f03fdcb7748f4
Michal Broczkowski <mibro@op.pl>
2016-09-04 18:22:02 +0200

    Finish user edit, update, index, and destroy actions

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
+    assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
@@ -12,7 +13,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
-    assert_select 'form[action="/signup"]'
   end
   test "valid signup information" do
     get signup_path
